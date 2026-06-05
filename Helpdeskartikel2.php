2e5c048ba45140f6d92d12bda9c725b9c6f518fd
Christopher Serlier <christopherserlier@gmail.com>
2026-06-05 14:44:18 +0200

    Frontend startup

--- Helpdeskartikel2.php
+++ Helpdeskartikel2.php
@@ -1,12 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="Style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rennaiscance IT</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+  <link rel="stylesheet" href="Style.css">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <nav class="navbar navbar-expand-sm navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="Index.php">Rennaiscance</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarID">
+                <div class="navbar-nav ms-auto">
+                    <a class="nav-link active" aria-current="page" href="Index.php">Home</a>
+                    <a class="nav-link" href="Weblog.php">Weblog</a>
+                    <a class="nav-link" href="Helpdesk.php">Helpdesk</a>
+                    <a class="nav-link" href="Contact.php">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <main class="container py-5">
+
+    </main>
+
+    <footer class="page-footer text-center text-white py-4">
+        <div class="container">
+            <p class="mb-3"></p>
+            <ul class="footer-links mb-0">
+                <li><a href="Weblog.php">Weblog</a></li>
+                <li><a href="Verwerkingsovereenkomst.php">Verwerkingsovereenkomst</a></li>
+                <li><a href="Privacypolicy.php">Privacypolicy</a></li>
+                <li><a href="Contact.php">Contact</a></li>
+            </ul>
+        </div>
+    </footer>
 </body>
 </html>
