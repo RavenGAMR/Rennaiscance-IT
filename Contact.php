64b175559e635c2c79fe2917b2374cb286f52f7b
DanielW <114386941+IkBenDaniel@users.noreply.github.com>
2026-06-08 12:53:48 +0200

    Google maps demo toegevoegd
    Google maps demo toegevoegd, moet nog contact informatie

--- Contact.php
+++ Contact.php
@@ -1,1 +1,35 @@
 
+<<<<<<< Updated upstream
+=======
+    <main class="container py-5">
+    <h1>Contact</h1>
+    <p>Heeft u een vraag over een product of dienst? Stel deze dan gerust. Bij ons kunt u gewoon op werkdagen tijdens kantooruren direct telefonisch terecht, of anders te allen tijde per email.</p>
+    <div id="googleMap" style="width:100%;height:400px;"></div>
+
+        <script>
+        function myMap() {
+        var mapProp= {
+          center:new google.maps.LatLng(52.0410868,4.3373716),
+          zoom:17,
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+        }
+        </script>
+    
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUonX-LQIj3uCjgabva1OhtlVab1s9EHA&callback=myMap"></script>
+    </main>
+
+    <footer class="page-footer text-center text-white py-4">
+        <div class="container">
+            <p class="mb-3"></p>
+            <ul class="footer-links mb-0">
+                <li><a href="Weblog.php">Weblog</a></li>
+                <li><a href="Verwerkingsovereenkomst.php">Verwerkingsovereenkomst</a></li>
+                <li><a href="Privacypolicy.php">Privacypolicy</a></li>
+                <li><a href="Contact.php">Contact</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
+>>>>>>> Stashed changes
