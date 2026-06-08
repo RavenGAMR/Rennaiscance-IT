03a9a6efc9eb5f5323ec39deed2c71ff9672923a
DanielW <114386941+IkBenDaniel@users.noreply.github.com>
2026-06-08 12:56:10 +0200

    Google maps demo toegevoegd
    Google maps demo toegevoegd, moet nog contact informatie

--- Contact.php
+++ Contact.php
@@ -1,9 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rennaiscance IT</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+  <link rel="stylesheet" href="Style.css">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="Index.php">Rennaiscance</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarID">
+                <div class="navbar-nav ms-auto">
+                    <a class="nav-link active" aria-current="page" href="Index.php">Home</a>
+                    <a class="nav-link" href="Weblog.php">Weblog</a>
+                    <a class="nav-link" href="Helpdesk.php">Helpdesk</a>
+                    <a class="nav-link" href="Contact.php">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
 
-<<<<<<< Updated upstream
-=======
     <main class="container py-5">
-    <h1>Contact</h1>
-    <p>Heeft u een vraag over een product of dienst? Stel deze dan gerust. Bij ons kunt u gewoon op werkdagen tijdens kantooruren direct telefonisch terecht, of anders te allen tijde per email.</p>
+    Heeft u een vraag over een product of dienst? Stel deze dan gerust. Bij ons kunt u gewoon op werkdagen tijdens kantooruren direct telefonisch terecht, of anders te allen tijde per email.
     <div id="googleMap" style="width:100%;height:400px;"></div>
 
         <script>
@@ -32,4 +58,3 @@
     </footer>
 </body>
 </html>
->>>>>>> Stashed changes
